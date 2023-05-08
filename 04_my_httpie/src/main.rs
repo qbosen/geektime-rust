45c8df4bb487462e98737a20dacdeb244171a641
qiubaisen <abosen@qq.com>
2023-05-08 16:55:40 +0800

    feat(httpie): check URL from clap args

--- 04_my_httpie/src/main.rs
+++ 04_my_httpie/src/main.rs
@@ -1,8 +1,14 @@
+use anyhow::{Ok, Result};
 use clap::{Args, Parser, Subcommand};
+use reqwest::Url;
 
 /// 一个用Rust实现的原生HTTPie工具
 #[derive(Parser, Debug)]
-#[command(name = "HTTPie", author = "Baisen Qiu <abosen@qq.com>", version = "1.0")]
+#[command(
+    name = "HTTPie",
+    author = "Baisen Qiu <abosen@qq.com>",
+    version = "1.0"
+)]
 struct Opts {
     #[command(subcommand)]
     subcmd: SubCommand,
@@ -16,13 +22,21 @@ enum SubCommand {
 
 #[derive(Args, Debug)]
 struct Get {
+    #[arg(value_parser=parse_url)]
     url: String,
 }
 #[derive(Args, Debug)]
 struct Post {
+    #[arg(value_parser=parse_url)]
     url: String,
     body: Vec<String>,
 }
+
+fn parse_url(url: &str) -> Result<String> {
+    url.parse::<Url>()?;
+    Ok(url.into())
+}
+
 /* 测试一下
 cargo run post httpbin.org/post a=1 b=2
 Opts { subcmd: Post(Post { url: "httpbin.org/post", body: ["a=1", "b=2"] }) }
