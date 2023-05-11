440f0fdc631e5984e8218fed8406d0d644f19017
qiubaisen <abosen@qq.com>
2023-05-12 00:16:04 +0800

    feat(thumbor): axum定义web服务

--- 05_my_thumbor/src/main.rs
+++ 05_my_thumbor/src/main.rs
@@ -1,19 +1,38 @@
+use axum::{extract::Path, http::StatusCode, routing::get, Router};
+use pb::ImageSpec;
+use percent_encoding::percent_decode_str;
+use serde::Deserialize;
+
 mod pb;
 
-struct ImageSpec {
-    specs: Vec<Spec>,
-}
-enum Spec {
-    Resize(Resize),
-    Crop(Crop),
+#[derive(Deserialize)]
+struct Params {
+    spec: String,
+    url: String,
 }
 
-struct Resize {
-    width: u32,
-    height: u32,
-}
-struct Crop {}
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
 
-fn main() {
-    println!("Hello, world!");
+    // build our application with a route
+    let app = Router::new().route("/image/:spec/:url", get(generate));
+
+    // run our app with hyper, listening globally on port 3000
+    let addr = "0.0.0.0:3000".parse().unwrap();
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// ./target/debug/my_httpie get "http://localhost:3000/image/CgoKCAjYBBCgBiADCgY6BAgUEBQKBDICCAM/https%3A%2F%2Fimages%2Epexels%2Ecom%2Fphotos%2F2470905%2Fpexels%2Dphoto%2D2470905%2Ejpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D2%26h%3D750%26w%3D1260"
+async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, StatusCode> {
+    let url = percent_decode_str(&url).decode_utf8_lossy();
+    let spec: ImageSpec = spec
+        .as_str()
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+    Ok(format!("url: {}\nspec: {:#?}", url, spec))
 }
